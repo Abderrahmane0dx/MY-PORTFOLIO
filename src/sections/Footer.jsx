5b9565a5c525f4f421cfdfa483da31093f09aae8
Abderrahmane0dx <benamaraabderrahmane2@gmail.com>
2025-07-28 22:55:22 +0100

    ADDING THE FOOTER

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { socialImgs } from "../constants";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer className="footer pt-6 border-t border-gray-50 opacity-40">
+      <div className="footer-container flex flex-col items-center gap-4">
+        {/* Terms & Conditions */}
+        <div className="text-center lg:text-start">
+          <p>Terms & Conditions</p>
+        </div>
 
-export default Footer
+        {/* Social Icons */}
+        <div className="socials lg:justify-center">
+          {socialImgs.map((img) => (
+            <a className="icon" target="_blank" href={img.url} key={img.url}>
+              <img src={img.imgPath} />
+            </a>
+          ))}
+        </div>
+
+        {/* Copyright */}
+        <div className="text-center lg:text-end">
+          <p>
+            © {new Date().getFullYear()} Abderrahmane | Benamara. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
